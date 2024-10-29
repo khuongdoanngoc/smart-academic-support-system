22b533682ef4628a857f2ddeb35eeb6e7c744c34
Doan Ngoc Khuong <khuong.dgnc@gmail.com>
2024-10-29 14:21:32 +0700

    feat(homepage): add final ui homepage

--- src/sections/Home/IntroductionSection/IntroductionSection.tsx
+++ src/sections/Home/IntroductionSection/IntroductionSection.tsx
@@ -1,23 +1,57 @@
 import styles from "./Introduction.module.scss";
 import classnames from "classnames/bind";
-import Background from "../../../assets/images/homepage.introduction.jpeg";
+import Background1 from "../../../assets/images/homepage.introduction.jpeg";
+import Background2 from "../../../assets/images/homepage.introduce2.jpeg";
+import Avatar2 from "../../../assets/images/homepage.avatar2.jpeg";
 import ShapeBackground from "../../../assets/images/homepage.shape.png";
 import HomeAvatar from "../../../assets/images/homepage.avatar.jpeg";
 import Pattern from "../../../assets/images/homepage.pattern.png";
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
-import { appear, slideInLeft, slideInRight } from "../../../utils/animations";
+import { appear, slideInRight } from "../../../utils/animations";
 
 const cx = classnames.bind(styles);
 
 export default function IntroductionSection() {
+    const [currentBackground, setCurrentBackground] = useState(Background1);
+    const [currentAvatar, setCurrentAvatar] = useState(HomeAvatar);
+    const [currentHeadTitle, setCurrentHeadTitle] =
+        useState<string>("TRƯỜNG ĐẠI HỌC");
+    const [currentTitle, setCurrentTitle] = useState<string[]>([
+        "Trường Đại học tư thục",
+        "Đầu tiên",
+        "Tại miền Trung",
+    ]);
+    const [currentDescription, setCurrentDescription] = useState<string>(
+        "Mang đến cho sinh viên một môi trường học tập hiện đại và năng động"
+    );
+    const [cycleIndex, setCycleIndex] = useState(0); // 0 là Background1/Avatar1, 1 là Background2/Avatar2
+
     const [isVisible, setIsVisible] = useState<boolean>(false);
     const sectionRef = useRef(null);
 
+    const leftVariants = {
+        visible: {
+            x: 0,
+            transition: {
+                type: "spring",
+                bounce: 0,
+                duration: 1.4,
+            },
+        },
+        hidden: {
+            x: -1000,
+            transition: {
+                type: "spring",
+                bounce: 0,
+                duration: 1.4,
+            },
+        },
+    };
     useEffect(() => {
         const options = {
             root: null,
-            rootMargin: "400px",
+            rootMargin: "200px",
             threshold: 0.5,
         };
         const observer = new IntersectionObserver((entries, observer) => {
@@ -38,13 +72,57 @@ export default function IntroductionSection() {
         };
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsVisible((prev) => !prev);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if (isVisible) {
+            setCycleIndex((prevIndex) => (prevIndex + 1) % 2); // Tăng cycleIndex và luân phiên giữa 0 và 1
+        }
+    }, [isVisible]);
+
+    useEffect(() => {
+        if (cycleIndex === 0) {
+            setCurrentBackground(Background1);
+            setCurrentAvatar(HomeAvatar);
+            setCurrentHeadTitle("TRƯỜNG ĐẠI HỌC");
+            setCurrentTitle([
+                "Trường Đại học tư thục",
+                "Đầu tiên",
+                "Tại miền Trung",
+            ]);
+            setCurrentDescription(
+                "Mang đến cho sinh viên một môi trường học tập hiện đại và năng động"
+            );
+        } else {
+            setCurrentBackground(Background2);
+            setCurrentAvatar(Avatar2);
+            setCurrentHeadTitle("TÀI LIỆU HỌC TẬP");
+            setCurrentTitle([
+                "Tổng hợp bài giảng, tài liệu",
+                "Đầy đủ nhất",
+                "Dành cho sinh viên",
+            ]);
+            setCurrentDescription(
+                "Với hàng ngàn bài giảng sinh động, bài tập thực hành đa dạng và đề thi mẫu sát với chương trình, bạn sẽ dễ dàng nắm vững mọi kiến thức."
+            );
+        }
+    }, [cycleIndex]);
+
     return (
         <div className={cx("introduction-section")}>
             <div className={cx("background")}>
                 <div className={cx("background-color-cover")}>
-                    <img
+                    <motion.img
+                        variants={appear}
+                        initial="hidden"
+                        animate={isVisible ? "visible" : "hidden"}
                         className={cx("background-img")}
-                        src={Background}
+                        src={currentBackground}
                         alt="bg"
                     />
                 </div>
@@ -66,23 +144,20 @@ export default function IntroductionSection() {
             <div className={cx("content-wrapper")}>
                 <div className={cx("content")}>
                     <motion.div
-                        variants={slideInLeft}
+                        variants={leftVariants}
                         initial="hidden"
                         animate={isVisible ? "visible" : "hidden"}
                         ref={sectionRef}
                         className={cx("titles")}>
                         <h2>
-                            TRƯỜNG ĐẠI HỌC <span>DUY TÂN</span>
+                            {currentHeadTitle} <span>DUY TÂN</span>
                         </h2>
                         <div>
-                            <h1>Trường Đại học tư thục</h1>
-                            <h1>Đầu tiên</h1>
-                            <h1>Tại miền Trung</h1>
+                            {currentTitle.map((title, index) => (
+                                <h1 key={index}>{title}</h1>
+                            ))}
                         </div>
-                        <p>
-                            Mang đến cho sinh viên một môi trường học tập hiện
-                            đại và năng động.
-                        </p>
+                        <p>{currentDescription}</p>
                         <button>TÌM HIỂU THÊM</button>
                     </motion.div>
                     <div className={cx("avatar")}>
@@ -91,7 +166,7 @@ export default function IntroductionSection() {
                             initial="hidden"
                             animate={isVisible ? "visible" : "hidden"}
                             ref={sectionRef}
-                            src={HomeAvatar}
+                            src={currentAvatar}
                             alt="avatar"
                         />
                     </div>
