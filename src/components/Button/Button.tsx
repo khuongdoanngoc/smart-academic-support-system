eb24016bd2f43bda2d9ab03446a77621245cea7c
Doan Ngoc Khuong <khuong.dgnc@gmail.com>
2024-11-04 09:36:52 +0700

    feat(chatbot-ai-support): add ux ai-support page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,59 +4,60 @@ import classnames from "classnames/bind";
 const cx = classnames.bind(styles);
 
 interface IButton {
-  text: string;
-  paddingY: number;
-  paddingX: number;
-  fontSize: number;
+    text: string;
+    paddingY: number;
+    paddingX: number;
+    fontSize: number;
+    onClick?: () => void;
 }
 interface submitButton {
-  titleButton: string;
-  isSubmitting: boolean;
-  padding: number;
-  fontsize: number;
-  borderRadius: number;
-  background: string;
+    titleButton: string;
+    isSubmitting: boolean;
+    padding: number;
+    fontsize: number;
+    borderRadius: number;
+    background: string;
 }
 export default function Button({
-  text,
-  paddingY,
-  paddingX,
-  fontSize,
+    text,
+    paddingY,
+    paddingX,
+    fontSize,
+    onClick,
 }: IButton) {
-  return (
-    <button
-      style={{
-        padding: `${paddingY}px ${paddingX}px`,
-        fontSize: `${fontSize}px`,
-      }}
-      className={cx("button-wrapper")}
-    >
-      {text}
-    </button>
-  );
+    return (
+        <button
+            onClick={onClick}
+            style={{
+                padding: `${paddingY}px ${paddingX}px`,
+                fontSize: `${fontSize}px`,
+            }}
+            className={cx("button-wrapper")}>
+            {text}
+        </button>
+    );
 }
 
 export const ButtonSubmit: React.FC<submitButton> = ({
-  titleButton,
-  isSubmitting,
-  padding,
-  fontsize,
-  borderRadius,
-  background,
+    titleButton,
+    isSubmitting,
+    padding,
+    fontsize,
+    borderRadius,
+    background,
 }) => {
-  return (
-    <button
-      type="submit"
-      disabled={isSubmitting}
-      className={cx("button")}
-      style={{
-        padding: `${padding}px`,
-        fontSize: `${fontsize}px`,
-        borderRadius: `${borderRadius}px`,
-        backgroundColor: `${background}`,
-      }}
-    >
-      {titleButton}
-    </button>
-  );
+    return (
+        <button
+            type="submit"
+            disabled={isSubmitting}
+            className={cx("button")}
+            style={{
+                padding: `${padding}px`,
+                fontSize: `${fontsize}px`,
+                borderRadius: `${borderRadius}px`,
+                backgroundColor: `${background}`,
+            }}>
+            {titleButton}
+        </button>
+    );
 };
