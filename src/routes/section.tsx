4a6c738eda4c8604f3b78577faf55f3ca8652412
CaoVanAn3k <caovanan3k@gmail.com>
2024-11-04 13:12:45 +0700

    push coode notication

--- src/routes/section.tsx
+++ src/routes/section.tsx
@@ -8,38 +8,39 @@ import Login from "../pages/Login";
 import ForgotPassWord from "../pages/ForgotPassWord";
 import { Register, UploadFile } from "../pages";
 import NewPassword from "../pages/NewPassword";
+import Notication from "../pages/Notication";
 
 export default function Router() {
-    const routes = useRoutes([
-        {
-            element: (
-                <Layout>
-                    <Outlet />
-                </Layout>
-            ),
-            children: [
-              { element: <HomePage />, index: true },
-              { path: "/login", element: <Login />, index: true },
-              { path: "/register", element: <Register />, index: true },
-              { path: "/forgotpass", element: <ForgotPassWord />, index: true },
-              { path: "/newpassword", element: <NewPassword />, index: true },
-            ],
-            
-        },
+  const routes = useRoutes([
+    {
+      element: (
+        <Layout>
+          <Outlet />
+        </Layout>
+      ),
+      children: [
+        { element: <HomePage />, index: true },
+        { path: "/login", element: <Login />, index: true },
+        { path: "/register", element: <Register />, index: true },
+        { path: "/forgotpass", element: <ForgotPassWord />, index: true },
+        { path: "/newpassword", element: <NewPassword />, index: true },
+      ],
+    },
 
-        {
-            element: (
-                <DocumentLayout>
-                    <Outlet />
-                </DocumentLayout>
-            ),
-            children: [
-                { path: "/document", element: <DocumentPage /> },
-                { path: "/document/directory", element: <DocumentPage /> },
-                { path: "/document/support", element: <SupportPage /> },
-                { path: "/document/uploadfile", element: <UploadFile />, index: true },
-            ],
-        },
-    ]);
-    return routes;
+    {
+      element: (
+        <DocumentLayout>
+          <Outlet />
+        </DocumentLayout>
+      ),
+      children: [
+        { path: "/document", element: <DocumentPage /> },
+        { path: "/document/directory", element: <DocumentPage /> },
+        { path: "/document/support", element: <SupportPage /> },
+        { path: "/document/uploadfile", element: <UploadFile />, index: true },
+        { path: "/document/notication", element: <Notication />, index: true },
+      ],
+    },
+  ]);
+  return routes;
 }
